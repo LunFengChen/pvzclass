ee1aae190169db99b7762820e54e61299a7d4fd3
LunFengChen <2292990278@qq.com>
2025-04-15 12:07:27 +0800

    测试传送门

--- pvzmain/pvzmain.cpp
+++ pvzmain/pvzmain.cpp
@@ -1,19 +1,36 @@
 #include "../pvzclass/pvzclass.h"
 #include "../pvzclass/Events/Events.h"
+#include "../pvzclass/Extensions.h"
 
 int main()
 {
 	DWORD pid = ProcessOpener::Open();
 	if (!pid) return 1;
 	PVZ::InitPVZ(pid);
-	while (PVZ::GetPVZApp().GameState != PVZGameState::MainMenu) Sleep(10);
-	PVZ::Memory::immediateExecute = true;
-	PVZ::Memory::InjectDll("pvzdll.dll");
-	PVZ::Memory::InvokeDllProc("init");
-	EnableBackgroundRunning();
-	DisableInitialLawnmover();
-	DisableIceLevelFailSound();
-	DrawPlantReanimEvent();
+	
+	if(PVZ::GetBoard().GetBaseAddress() != 0){
+		while(true){
+			std::string s;
+			std::cin >> s;
+
+			if (!s.compare("1")) {
+				EnablePortal();
+				continue;
+			}
+			else if (!s.compare("2")) {
+				EnablePortal(ThreeState::Enable);
+				Creator::AsmInit();
+				Creator::CreatePortal(3, 5, 4, 0, 0, 1, 0, 5);
+				Creator::AsmReset();
+				continue;
+			}
+			else if (!s.compare("3")) {
+				EnablePortal(ThreeState::Disable);
+				continue;
+			}
+		}
+	}
+
 	PVZ::QuitPVZ();
 	return 0;
 }
