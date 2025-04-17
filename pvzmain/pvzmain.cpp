52e6f3392d986010d0c18f4e94bdad4cd9cc6c8c
LunFengChen <2292990278@qq.com>
2025-04-17 22:48:19 +0800

    测试cratecaption

--- pvzmain/pvzmain.cpp
+++ pvzmain/pvzmain.cpp
@@ -7,29 +7,32 @@ int main()
 	DWORD pid = ProcessOpener::Open();
 	if (!pid) return 1;
 	PVZ::InitPVZ(pid);
-	
-	if(PVZ::GetBoard().GetBaseAddress() != 0){
-		while(true){
-			std::string s;
-			std::cin >> s;
 
-			if (!s.compare("1")) {
-				EnablePortal();
-				continue;
-			}
-			else if (!s.compare("2")) {
-				EnablePortal(ThreeState::Enable);
-				Creator::AsmInit();
-				Creator::CreatePortal(3, 5, 4, 0, 0, 1, 0, 5);
-				Creator::AsmReset();
-				continue;
-			}
-			else if (!s.compare("3")) {
-				EnablePortal(ThreeState::Disable);
-				continue;
-			}
-		}
-	}
+	std::string caption_str = "123restart";
+	Creator::CreateCaption(caption_str.c_str(), 11, CaptionStyle::Lowermiddle);
+	caption_str = "123";
+	Creator::CreateCaption(caption_str.c_str(), 4, CaptionStyle::BottomWhite);
+
+	Sleep(6000);
+	std::string str1 = "123restart";
+	Creator::CreateCaption(str1.c_str(), str1.size(), CaptionStyle::Lowermiddle);
+	std::string str2 = "123";
+	Creator::CreateCaption(str2.c_str(), str2.size(), CaptionStyle::Lowermiddle);
+
+
+	Sleep(6000);
+	std::string str3 = "123restart";
+	Creator::CreateCaption(str3.c_str(), str3.size()+1, CaptionStyle::Lowermiddle);
+	std::string str4 = "123";
+	Creator::CreateCaption(str4.c_str(), str4.size()+1, CaptionStyle::Lowermiddle);
+
+
+	Sleep(6000);
+	const char str5[] = "123restart\0";
+	Creator::CreateCaption(str5, 13, CaptionStyle::Lowermiddle);
+
+	const char str6[] = { '1', '2', '3', '\0' };
+	Creator::CreateCaption(str6, 4, CaptionStyle::Lowermiddle);
 
 	PVZ::QuitPVZ();
 	return 0;
